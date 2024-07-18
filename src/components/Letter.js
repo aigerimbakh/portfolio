8481760f745fddfda7e06add4b28fa3e2b9c9380
aigerimbakh <milashkalovec@gmail.com>
2024-07-18 16:01:12 +0500

    siiix

---
+++ src/components/Letter.js
@@ -0,0 +1,47 @@
+import { useState, useEffect } from "react";
+import { Col, Row, Alert } from "react-bootstrap";
+
+export const Newsletter = ({ status, message, onValidated }) => {
+  const [email, setEmail] = useState('');
+
+  useEffect(() => {
+    if (status === 'success') clearFields();
+  }, [status])
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    email &&
+    email.indexOf("aigerimbakht@gmail.com") > -1 &&
+    onValidated({
+      EMAIL: email
+    })
+  }
+
+  const clearFields = () => {
+    setEmail('');
+  }
+
+  return (
+    <Col lg={12}>
+      <div className="newsletter-bx wow slideInUp">
+        <Row>
+          <Col lg={12} md={6} xl={5}>
+            <h3>Subscribe to our Newsletter<br /> & Never miss latest updates</h3>
+            {status === 'sending' && <Alert>Sending...</Alert>}
+            {status === 'error' && <Alert variant="danger">{message}</Alert>}
+            {status === 'success' && <Alert variant="success">{message}</Alert>}
+          </Col>
+          <Col md={9} xl={6}>
+  <form onSubmit={handleSubmit}>
+    <div className="new-email-bx">
+      <input value={email} type="email" onChange={(e) => setEmail(e.target.value)} placeholder="Email " />
+      <button type="submit" className="btn btn-primary">Submit</button>
+    </div>
+  </form>
+</Col>
+
+        </Row>
+      </div>
+    </Col>
+  )
+}
